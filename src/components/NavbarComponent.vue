71bc81874e83e431831e9795c7b48e6465485076
MuchiriAndrew <kariukia225@gmail.com>
2023-08-07 17:28:35 +0300

    update

--- src/components/NavbarComponent.vue
+++ src/components/NavbarComponent.vue
@@ -11,11 +11,15 @@
     </a>
     </transition>
 
-    <div v-if="show" style="border:none" class="navbar-toggler" data-bs-toggle="offcanvas" data-bs-target="#offcanvasNavbar" aria-controls="offcanvasNavbar" aria-expanded="false" aria-label="Toggle navigation">
-      <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="#0075FF" class="bi bi-list" viewBox="0 0 16 16">
+
+    <div style="border:none" class="navbar-toggler" data-bs-toggle="offcanvas" data-bs-target="#offcanvasNavbar" aria-controls="offcanvasNavbar" aria-expanded="false" aria-label="Toggle navigation">
+      <transition name="toggle">
+    <svg v-if="show" xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="#0075FF" class="bi bi-list" viewBox="0 0 16 16">
       <path fill-rule="evenodd" d="M2.5 12a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5z"/>
     </svg>
+    </transition>
     </div>
+
 
     <div class="offcanvas offcanvas-end" tabindex="-1" id="offcanvasNavbar" aria-labelledby="offcanvasNavbarLabel">
 
@@ -108,6 +112,9 @@ export default {
 .navfade-enter-from{opacity: 0;transform:translateY(-20px) ;}
 .navfade-enter-to{opacity: 1;}
 .navfade-enter-active{transition: all 0.8s ease-in-out;}
+
+.toggle-enter-from{transform:translateX(20px) ;}
+.toggle-enter-active{transition: 0.8s ease-in-out;}
 
 * {
   font-family: Fira Code;
