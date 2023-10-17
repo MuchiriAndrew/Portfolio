dc4653e0aca461563944203413f6f3a44a68a900
MuchiriAndrew <kariukia225@gmail.com>
2023-10-17 20:36:42 +0300

    update

--- src/components/About.vue
+++ src/components/About.vue
@@ -29,26 +29,24 @@
                 },
             }" id="txt-wrapper">
 
-            <div id="aboutrow" class="bg-success">
+            <div id="aboutrow" class="">
                 <div id="txt-col" class="">
                 <p>Hello, my name is Andrew Muchiri. I am a software engineer based in Nairobi, Kenya. I am 20 years old and currently a third year Computer Science student at Kenyatta University. I enjoy coming up with creative solutions to complex issues using various technologies. My love for writing code began when I started studying Computer Science and voila! I guess I found my passion. Besides creating beautiful and efficient web applications, I am an avid chess player and occassionally take part in chess tournaments where I have a blast! <br> I aspire to be a renowned 10x developer who prioritizes delivering softwares that exceed client expectations.</p>
                 </div>
 
-                <div id="img-col" class="bg-primary">
+                <div id="img-col" class="">
                     <img id="myimg" class="img-fluid rounded-3" src="../assets/myimg.jpg" alt="me">
                 </div>
             </div>
 
             <div id="heading">
-                <h4>My Skills</h4>
+                <h4 class="mt-5">MY SKILLS</h4>
             </div>
 
-
-
             <div id="skills" class="mt-3">
-                    <div id="col1" class="bg-primary">
+                    <div id="col1" class="">
                         <ul class="p-0" style="list-style:none" id="frontend">
-                            <h5>Frontend Technologies</h5>
+                            <h5>FRONTEND TECHNOLOGIES</h5>
                             <li> <svg xmlns="http://www.w3.org/2000/svg" width="16" height="13" fill="#64ffda" class="bi bi-caret-right" viewBox="0 0 16 16">
                             <path d="M6 12.796V3.204L11.481 8 6 12.796zm.659.753 5.48-4.796a1 1 0 0 0 0-1.506L6.66 2.451C6.011 1.885 5 2.345 5 3.204v9.592a1 1 0 0 0 1.659.753z"/>
                             </svg> HTML/CSS</li>
@@ -71,9 +69,9 @@
 
                     </div>
 
-                    <div id="col2" class="bg-success">
+                    <div id="col2" class="">
                         <ul class="p-0" style="list-style:none" id="backend">
-                            <h5>Backend Technologies</h5>
+                            <h5>BACKEND TECHNOLOGIES</h5>
                            <li> <svg xmlns="http://www.w3.org/2000/svg" width="16" height="13" fill="#64ffda" class="bi bi-caret-right" viewBox="0 0 16 16">
                             <path d="M6 12.796V3.204L11.481 8 6 12.796zm.659.753 5.48-4.796a1 1 0 0 0 0-1.506L6.66 2.451C6.011 1.885 5 2.345 5 3.204v9.592a1 1 0 0 0 1.659.753z"/>
                             </svg> Nodejs</li>
@@ -87,9 +85,9 @@
 
                     </div>
 
-                    <div id="col3" class="bg-warning">
+                    <div id="col3" class="">
                         <ul class="p-0" style="list-style:none">
-                            <h5>Databases, Tools and Platforms</h5>
+                            <h5>DATABASES, TOOLS AND PLATFORMS</h5>
                             <li> <svg xmlns="http://www.w3.org/2000/svg" width="16" height="13" fill="#64ffda" class="bi bi-caret-right" viewBox="0 0 16 16">
                             <path d="M6 12.796V3.204L11.481 8 6 12.796zm.659.753 5.48-4.796a1 1 0 0 0 0-1.506L6.66 2.451C6.011 1.885 5 2.345 5 3.204v9.592a1 1 0 0 0 1.659.753z"/>
                             </svg> SQL</li>
@@ -143,8 +141,8 @@ export default {
 <style scoped>
 
 #wrapper2 {
-    /* background: #0a192f; */
-    background: aqua;
+    background: #0a192f;
+    /* background: aqua; */
     min-height: max-content;
     display: flex;
     flex-direction: column;
@@ -170,7 +168,6 @@ export default {
 }
 
 #txt-wrapper {
-    background: pink;
     height:120vh;
     min-height: max-content;
     width: 90%;
@@ -199,10 +196,18 @@ export default {
     font-family: Poppins;
 }
 
-#skills {
-    height: 40%;
+#img-col {
+    width: 50%;
+    height: 100%;
     display: flex;
-    flex-direction: row;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+}
+
+#myimg {
+    height: 100%;
+    box-shadow: #0a192f 10px -10px 0px -3px, #64ffda 10px -10px;
 }
 
 #heading {
@@ -211,13 +216,18 @@ export default {
     justify-content: center;
     align-items: center;
     height: max-content;
-    background: red;
 }
 
 #heading h4 {
     color: #64ffda;
     font-size: 28px;
     font-family: Fira Code;
+}
+
+#skills {
+    height: 40%;
+    display: flex;
+    flex-direction: row;
 }
 
 #col1 {
@@ -250,166 +260,416 @@ ul {
 
 h5 {
     color: #64ffda;
+    font-size: 14px;
 }
 
 li {
     color:hsl(239, 57%, 85%);
 }
 
-#img-col {
-    width: 50%;
-    height: 100%;
-    display: flex;
-    flex-direction: column;
-    justify-content: center;
-    align-items: center;
-}
-
-#myimg {
-    height: 100%;
-    box-shadow: #0a192f 10px -10px 0px -3px, #64ffda 10px -10px;
-}
-
 @media (max-width: 992px) {
 #txt-wrapper {
-    height: 60%;
-    width: 90%;
+    min-height: max-content;
+    height: 70vh;
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+}
+
+#aboutrow {
+    width:100%;
+    height: 50%;
     display: flex;
     flex-direction: row;
-    justify-content: center;
-    align-items: center;
 }
 
 #txt-col {
     width: 50%;
-    height: 80%;
+    height: 100%;
     display: flex;
     flex-direction: column;
-    justify-content: flex-start;
+    justify-content: center;
 }
-    #img-col {
+
+#txt-col p {
+    color: hsl(223, 19%, 93%);
+    font-size: 16px;
+    font-family: Poppins;
+}
+
+#img-col {
     width: 50%;
-    height: 800px;
+    /* height: 100%; */
     display: flex;
     flex-direction: column;
-    justify-content: flex-start;
+    justify-content: center;
     align-items: center;
 }
 
 #myimg {
-    width: 300px;
     height: 300px;
+    width: 300px;
+    box-shadow: #0a192f 10px -10px 0px -3px, #64ffda 10px -10px;
 }
 
-#myimg:hover {
-   scale: unset;
-   transition: unset;
+#skills {
+    height: 40%;
+    display: flex;
+    flex-direction: row;
+}
+
+#col1 {
+    width: 33.333%;
+    height: 100%;
+    display: flex;
+    justify-content: center;
+}
+
+#col2 {
+    width: 33.333%;
+    height: 100%;
+    display: flex;
+    justify-content: center;
+}
+#col3 {
+    width: 33.333%;
+    height: 100%;
+    display: flex;
+    justify-content: center;
+}
+
+ul {
+    height: 100%;
+    width: 70%;
+    /* text-align: center; */
+    display: flex;
+    flex-direction: column;
+    align-items: flex-start;
 }
 }
 
 @media (max-width: 992px) and (max-height: 500px)  {
-    #txt-wrapper {
-    height: 80%;
-    width: 90%;
+#txt-wrapper {
+    min-height: max-content;
+    height: 180vh;
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+}
+
+#aboutrow {
+    width:100%;
+    height: 50%;
     display: flex;
     flex-direction: row;
-    justify-content: center;
-    align-items: center;
 }
 
 #txt-col {
     width: 50%;
-    height: 80%;
+    height: 100%;
     display: flex;
     flex-direction: column;
-    justify-content: flex-start;
+    justify-content: center;
 }
-    #img-col {
+
+#txt-col p {
+    color: hsl(223, 19%, 93%);
+    font-size: 16px;
+    font-family: Poppins;
+}
+
+#img-col {
     width: 50%;
-    height: 800px;
+    /* height: 100%; */
     display: flex;
     flex-direction: column;
-    justify-content: flex-start;
+    justify-content: center;
     align-items: center;
 }
 
 #myimg {
-    width: 300px;
     height: 300px;
+    width: 300px;
+    box-shadow: #0a192f 10px -10px 0px -3px, #64ffda 10px -10px;
+}
+
+#skills {
+    height: 40%;
+    display: flex;
+    flex-direction: row;
+}
+
+#col1 {
+    width: 33.333%;
+    height: 100%;
+    display: flex;
+    justify-content: center;
+}
+
+#col2 {
+    width: 33.333%;
+    height: 100%;
+    display: flex;
+    justify-content: center;
+}
+#col3 {
+    width: 33.333%;
+    height: 100%;
+    display: flex;
+    justify-content: center;
+}
+
+ul {
+    height: 100%;
+    width: 70%;
+    /* text-align: center; */
+    display: flex;
+    flex-direction: column;
+    align-items: flex-start;
 }
 
 }
 
 @media (max-width: 768px) {
-#wrapper2 {
-    background: #0a192f;
-    min-height: 140vh;
-    height: max-content;
-    display: flex;
-    flex-direction: column;
-    justify-content: center;
-    align-items: center;
-    text-align: start;
-}
-
 #txt-wrapper {
-    height: max-content;
+    /* min-height: max-content; */
+    height: 170vh;
     width: 90%;
     display: flex;
     flex-direction: column;
-    justify-content: unset;
-    align-items:unset;
+    justify-content: space-evenly;
+}
+
+#aboutrow {
+    width:100%;
+    height: 50%;
+    display: flex;
+    flex-direction: column;
 }
 
 #txt-col {
     width: 100%;
-    height: 60%;
+    height: 100%;
     display: flex;
     flex-direction: column;
     justify-content: center;
-    padding-top: 20px;
+}
+
+#txt-col p {
+    color: hsl(223, 19%, 93%);
+    font-size: 16px;
+    font-family: Poppins;
 }
 
 #img-col {
-    padding-top: 30px;
-    padding-bottom: 30px;
     width: 100%;
-    height: 40%;
     display: flex;
     flex-direction: column;
-    justify-content: flex-end;
+    justify-content: center;
     align-items: center;
 }
 
 #myimg {
-    width: 250px;
-    height: 250px;
+    height: 300px;
+    width: 300px;
+    box-shadow: #0a192f 10px -10px 0px -3px, #64ffda 10px -10px;
+}
+
+#skills {
+    height: 40%;
+    display: flex;
+    flex-direction: column;
+    justify-content: space-around;
+}
+
+#col1 {
+    width: 100%;
+    height: max-content;
+    display: flex;
+    justify-content: center;
+}
+
+#col2 {
+    width: 100%;
+    height: max-content;
+    display: flex;
+    justify-content: center;
+}
+#col3 {
+    width: 100%;
+    height: max-content;
+    display: flex;
+    justify-content: center;
+}
+
+ul {
+    height: 100%;
+    width: 60%;
+    display: flex;
+    /* text-align: start; */
+    flex-direction: column;
+    align-items: flex-start;
 }
 }
 
 @media screen and (max-height:720px) and (max-width:768px) {
-#wrapper2 {
-    background: #0a192f;
-    min-height: 190vh;
-    height: max-content;
+#txt-wrapper {
+    height: 250vh;
+    width: 90%;
+    display: flex;
+    flex-direction: column;
+    justify-content: space-evenly;
+}
+
+#aboutrow {
+    width:100%;
+    height: 50%;
+    display: flex;
+    flex-direction: column;
+}
+
+#txt-col {
+    width: 100%;
+    height: 100%;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+}
+
+#txt-col p {
+    color: hsl(223, 19%, 93%);
+    font-size: 16px;
+    font-family: Poppins;
+}
+
+#img-col {
+    width: 100%;
     display: flex;
     flex-direction: column;
     justify-content: center;
     align-items: center;
-    text-align: start;
-}
-
-#txt-wrapper {
-    height: max-content;
-}
-
-#img-col {
-    justify-content: center;
 }
 
 #myimg {
-    width: 250px;
-    height: 250px;
+    height: 300px;
+    width: 300px;
+    box-shadow: #0a192f 10px -10px 0px -3px, #64ffda 10px -10px;
+}
+
+#skills {
+    height: 40%;
+    display: flex;
+    flex-direction: column;
+    justify-content: space-around;
+}
+
+#col1 {
+    width: 100%;
+    height: max-content;
+    display: flex;
+    justify-content: center;
+}
+
+#col2 {
+    width: 100%;
+    height: max-content;
+    display: flex;
+    justify-content: center;
+}
+#col3 {
+    width: 100%;
+    height: max-content;
+    display: flex;
+    justify-content: center;
+}
+
+ul {
+    height: 100%;
+    width: 60%;
+    display: flex;
+    /* text-align: center; */
+    flex-direction: column;
+    align-items: flex-start;
+}
+}
+@media screen and (max-height:400px) and (max-width:768px) {
+#txt-wrapper {
+    height: 350vh;
+    width: 90%;
+    display: flex;
+    flex-direction: column;
+    justify-content: space-evenly;
+}
+
+#aboutrow {
+    width:100%;
+    height: 50%;
+    display: flex;
+    flex-direction: column;
+}
+
+#txt-col {
+    width: 100%;
+    height: 100%;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+}
+
+#txt-col p {
+    color: hsl(223, 19%, 93%);
+    font-size: 16px;
+    font-family: Poppins;
+}
+
+#img-col {
+    width: 100%;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+}
+
+#myimg {
+    height: 300px;
+    width: 300px;
+    box-shadow: #0a192f 10px -10px 0px -3px, #64ffda 10px -10px;
+}
+
+#skills {
+    height: 40%;
+    display: flex;
+    flex-direction: column;
+    justify-content: space-around;
+}
+
+#col1 {
+    width: 100%;
+    height: max-content;
+    display: flex;
+    justify-content: center;
+}
+
+#col2 {
+    width: 100%;
+    height: max-content;
+    display: flex;
+    justify-content: center;
+}
+#col3 {
+    width: 100%;
+    height: max-content;
+    display: flex;
+    justify-content: center;
+}
+
+ul {
+    height: 100%;
+    width: 60%;
+    display: flex;
+    /* text-align: center; */
+    flex-direction: column;
+    align-items: flex-start;
 }
 }
 
