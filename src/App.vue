c178c98548fa09950393e4d762accda588100f16
Muchiri <121347385+MuchiriAndrew@users.noreply.github.com>
2025-01-18 22:35:05 +0300

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -2,6 +2,7 @@
   <router-view />
 </template>
 
+
 <style>
   @import url('https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;700;900&family=Rubik:wght@400;500;700&display=swap');
   @import url('https://fonts.googleapis.com/css2?family=Fira+Code:wght@300;400;500;600;700&display=swap');
