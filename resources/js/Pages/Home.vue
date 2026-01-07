9026b7558f82a0d130cd78660bffd6d497a726b2
MuchiriAndrew <kariukia225@gmail.com>
2026-01-07 13:49:44 +0300

    init v2

---
+++ resources/js/Pages/Home.vue
@@ -0,0 +1,13 @@
+<template>
+    <div class="">
+        <h1 class="text-4xl font-bold text-primary">Portfolio v2</h1>
+
+        <h3 class="text-2xl font-bold text-secondary">Newest portfolio</h3>
+    </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style scoped></style>
